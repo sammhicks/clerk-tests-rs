abcb6a29b73193d1a9c75286fc3cef0a11f2eb40
Samuel Hicks <SamMHicks@hotmail.co.uk>
2020-07-27 13:32:18 +0100

    Implemented verbose logging

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,41 @@
 use clerk::{DataPins4Lines, Pins};
 
-struct FakeLine;
+struct FakeLine(&'static str);
 
 impl clerk::DisplayHardwareLayer for FakeLine {
-    fn set_level(&self, _: clerk::Level) {}
-    fn set_direction(&self, _: clerk::Direction) {}
+    fn set_level(&self, level: clerk::Level) {
+        println!("Setting {} (Fake) to {:?}", self.0, level);
+    }
+    fn set_direction(&self, direction: clerk::Direction) {
+        println!("Setting {} (Fake) to {:?}", self.0, direction);
+    }
     fn get_value(&self) -> u8 {
+        println!("Reading from {} (Fake)", self.0);
         0
     }
 }
 
-struct Line(gpio_cdev::LineHandle);
+struct Line {
+    consumer: &'static str,
+    handle: gpio_cdev::LineHandle,
+}
 
 impl clerk::DisplayHardwareLayer for Line {
     fn set_level(&self, level: clerk::Level) {
-        self.0
+        println!("Setting {} to {:?}", self.consumer, level);
+        self.handle
             .set_value(match level {
                 clerk::Level::Low => 0,
                 clerk::Level::High => 1,
             })
             .unwrap();
     }
-    fn set_direction(&self, _: clerk::Direction) {}
+    fn set_direction(&self, direction: clerk::Direction) {
+        println!("Setting {} to {:?}", self.consumer, direction);
+    }
 
     fn get_value(&self) -> u8 {
+        println!("Reading from {}", self.consumer);
         0
     }
 }
@@ -31,8 +43,19 @@ impl clerk::DisplayHardwareLayer for Line {
 struct Delay;
 
 impl clerk::Delay for Delay {
-    fn delay_ns(delay: u16) {
-        std::thread::sleep(std::time::Duration::from_nanos(delay as u64));
+    const ADDRESS_SETUP_TIME: u16 = 60;
+    const ENABLE_PULSE_WIDTH: u16 = 450;
+    const DATA_HOLD_TIME: u16 = 20;
+    const COMMAND_EXECUTION_TIME: u16 = 37;
+
+    fn delay_ns(ns: u16) {
+        println!("Sleeping for {} ns", ns);
+        std::thread::sleep(std::time::Duration::from_nanos(ns as u64));
+    }
+
+    fn delay_us(us: u16) {
+        println!("Sleeping for {} µs", us);
+        std::thread::sleep(std::time::Duration::from_micros(us as u64));
     }
 }
 
@@ -41,11 +64,10 @@ fn get_line(
     offset: u32,
     consumer: &'static str,
 ) -> Result<Line, gpio_cdev::errors::Error> {
-    Ok(Line(chip.get_line(offset)?.request(
-        gpio_cdev::LineRequestFlags::OUTPUT,
-        0,
-        consumer,
-    )?))
+    let handle =
+        chip.get_line(offset)?
+            .request(gpio_cdev::LineRequestFlags::OUTPUT, 0, consumer)?;
+    Ok(Line { consumer, handle })
 }
 
 fn main() -> Result<(), gpio_cdev::errors::Error> {
@@ -58,7 +80,7 @@ fn main() -> Result<(), gpio_cdev::errors::Error> {
     let mut chip = gpio_cdev::Chip::new("/dev/gpiochip0")?;
 
     let register_select = get_line(&mut chip, 17, "register_select")?;
-    let read = FakeLine;
+    let read = FakeLine("read");
     let enable = get_line(&mut chip, 27, "enable")?;
     let data4 = get_line(&mut chip, 22, "data4")?;
     let data5 = get_line(&mut chip, 23, "data5")?;
@@ -80,9 +102,11 @@ fn main() -> Result<(), gpio_cdev::errors::Error> {
     let mut lcd: clerk::Display<_, clerk::DefaultLines> =
         clerk::Display::new(pins.into_connection::<Delay>());
 
-    lcd.init(clerk::FunctionSetBuilder::default().set_line_number(clerk::LineNumber::One));
+    println!("init");
 
-    std::thread::sleep(std::time::Duration::from_millis(500));
+    lcd.init(clerk::FunctionSetBuilder::default().set_line_number(clerk::LineNumber::Two));
+
+    println!("set_display_control");
 
     lcd.set_display_control(
         clerk::DisplayControlBuilder::default()
@@ -91,14 +115,12 @@ fn main() -> Result<(), gpio_cdev::errors::Error> {
             .set_cursor_blinking(clerk::CursorBlinking::On),
     );
 
-    std::thread::sleep(std::time::Duration::from_millis(500));
+    println!("clear");
 
     lcd.clear();
 
-    std::thread::sleep(std::time::Duration::from_millis(500));
-
     for c in message.chars() {
-        std::thread::sleep(std::time::Duration::from_millis(100));
+        println!("write");
         lcd.write(c as u8);
     }
 
